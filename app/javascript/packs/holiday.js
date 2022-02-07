177154fd41fa33e1c517e719b3e243817253651b
Drbeer-miyako <80018151+Drbeer-miyako@users.noreply.github.com>
2022-02-07 11:46:52 +0900

    jsからHOLIDAYを削除

--- app/javascript/packs/holiday.js
+++ app/javascript/packs/holiday.js
@@ -1,20 +1,20 @@
-const holiday = () => {
+// const holiday = () => {
 
 
-}
+// }
    
-window.addEventListener('load', holiday);
+// window.addEventListener('load', holiday);
 
-function holiday (){
-     const  acquisitionDays = document.getElementById("holiday_acquisition_days");
-     acquisitionDays.addEventListener("keyup", () => {
-       const fee = Math.floor(itemPrice.value * 0.1);
-       const addTaxPrice = document.getElementById("add-tax-price");
-       addTaxPrice.innerHTML = `${fee}`;
-       const SalesProfit = itemPrice.value - fee ;
-       const profit = document.getElementById("profit");
-       profit.innerHTML = `${SalesProfit}`;
-     });
-}
+// function holiday (){
+//      const  acquisitionDays = document.getElementById("holiday_acquisition_days");
+//      acquisitionDays.addEventListener("keyup", () => {
+//        const fee = Math.floor(itemPrice.value * 0.1);
+//        const addTaxPrice = document.getElementById("add-tax-price");
+//        addTaxPrice.innerHTML = `${fee}`;
+//        const SalesProfit = itemPrice.value - fee ;
+//        const profit = document.getElementById("profit");
+//        profit.innerHTML = `${SalesProfit}`;
+//      });
+// }
    
-window.addEventListener('load', holiday);
+// window.addEventListener('load', holiday);
