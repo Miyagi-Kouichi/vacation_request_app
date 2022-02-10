11247dc68551da9e211802a94f72e12a0fbc6838
Drbeer-miyako <80018151+Drbeer-miyako@users.noreply.github.com>
2022-02-10 16:03:18 +0900

    db error fix1

--- db/schema.rb
+++ db/schema.rb
@@ -100,8 +100,6 @@ ActiveRecord::Schema.define(version: 2021_12_31_082221) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "week_h_checks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
