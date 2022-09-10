3242546672d36b04baee708e6898ad99b320854b
Drbeer-miyako <80018151+Drbeer-miyako@users.noreply.github.com>
2022-09-10 13:45:43 +0900

    グループのメンバー追加機能のエラーの解消５

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,7 @@
 class Group < ApplicationRecord
      has_many :group_users
      has_many :notifications
-     has_many :users, through: :group_users, dependent: :destroy
+     has_many :users, through: :group_users
      accepts_nested_attributes_for :group_users
      # has_many :users, through: :notifications
      validates :group_name, presence: true, uniqueness: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
-  has_many :group_users, dependent: :destroy
+  has_many :group_users
   has_many :groups, through: :group_users
   has_many :requests, dependent: :destroy
   has_many :holidays , dependent: :destroy
